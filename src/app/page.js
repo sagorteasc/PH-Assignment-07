c4190df36e8571753b294fdc171e2b49a64e3837
Md Naimul Islam Sagor <islamsagor49368@gmail.com>
2026-04-16 21:45:39 +0600

    add loader while fetching data

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,18 @@
 import Banner from "@/components/Banner/Banner";
 import Friends from "@/components/Friends/Friends";
+import { Suspense } from "react";
+import { PulseLoader } from "react-spinners";
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <hr className="text-[#E9E9E9] my-10 w-[70%] mx-auto" />
-      <Friends />
+
+      <Suspense fallback={<span className="flex justify-center items-center h-40"><PulseLoader color="#36d7b7" />
+      </span>}>
+        <Friends />
+      </Suspense>
     </div>
   );
 };
